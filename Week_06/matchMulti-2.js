b20c4fa124de41240ece91a08843311071e118f8
zhangyoung99 <zhangyoung11@gmail.com>
2020-10-03 10:37:00 +0800

    状态机处理string

---
+++ Week_06/matchMulti-2.js
@@ -0,0 +1,54 @@
+function matchMulti(str) {
+    let state = start;
+    for(let c of str) {
+        state = state(c);
+    }
+    return state === end;
+
+}
+function start(c) {
+    if( c === 'a')
+       return foundA;
+    else
+       return start;
+}
+
+function end(c) {
+    return end;
+}
+
+function foundA(c) {
+    if( c === 'b')
+       return foundB;
+    else 
+      return start(c); // reConsume
+}
+
+function foundB(c) {
+    if(c === 'c')
+       return foundC
+    else 
+       return start(c)
+}
+
+function foundC(c) {
+    if(c === 'd')
+       return foundD
+    else 
+       return start(c)
+}
+
+function foundD(c) {
+    if(c === 'e')
+       return foundE
+    else 
+       return start(c)
+}
+
+function foundE(c) {
+    if(c === 'f')
+       return end;
+    else 
+       return start(c);
+}
+match('I wabcdefghhh')
