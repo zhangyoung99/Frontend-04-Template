2961a7e27eb8685ba0551f64790e5c7802f92613
zhangyoung99 <zhangyoung11@gmail.com>
2020-10-06 11:19:16 +0800

    response解析

--- Week_06/client.js
+++ Week_06/client.js
@@ -56,14 +56,71 @@ class Request {
 }
 
 class ResponseParser {
-    constructor() {}
+    constructor() {
+        this.WAITING_STATUS_LINE = 0
+        this.WAITING_STATUS_LINE_END = 1
+        this.WAITING_HEADER_NAME = 2
+        this.WAITING_HEADER_SPACE = 3
+        this.WAITING_HEADER_VALUE = 4
+        this.WAITING_HEADER_LINE_END = 5
+        this.WAITING_HEADER_BLOCK_END = 6
+        this.WAITING_BODY = 7
+
+        this.current = this.WAITING_STATUS_LINE
+        this.statusLine = ''
+        this.headers = {}
+        this.headerName = ''
+        this.headerValue = ''
+        this.bodyParser = null
+    }
     receive(string) {
         for(let i=0; i < string.length; i++) {
             this.receiverChar(string.charAt(i))
         }
     }
     receiveChar(char) {
-
+        if(this.current === this.WAITING_STATUS_LINE) {
+            if(char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END
+            } else {
+                this.statusLine += char
+            }
+        } else if(this.current === this.WAITING_STATUS_LINE_END) {
+            if(char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if(this.current === this.WAITING_HEADER_NAME) {
+            if(char === ':') {
+                this.current = this.WAITING_HEADER_SPACE
+            } else if(char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END
+            } else {
+                this.headerName += char
+            }
+        } else if(this.current === this.WAITING_HEADER_SPACE) {
+            if(char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE
+            }
+        } else if(this.current === this.WAITING_HEADER_VALUE) {
+            if(char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END
+                this.headers[this.headerName] = this.headerValue
+                this.headerName = ''
+                this.headerValue = ''
+            } else {
+                this.headerValue += char
+            }
+        } else if(this.current === this.WAITING_HEADER_LINE_END) {
+            if(char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if(this.current === this.WAITING_HEADER_BLOCK_END) {
+            if(char === '\n') {
+                this.current = this.WAITING_BODY
+            }
+        } else if(this.current === this.WAITING_BODY) {
+            console.log(char)
+        }
     }
 }
 
