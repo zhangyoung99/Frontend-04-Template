c8213b07650d1eb9ab988ac40f5784f6b8dd418e
zhangyoung99 <zhangyoung11@gmail.com>
2020-10-06 10:12:59 +0800

    发送请求函数设计

--- Week_06/server.js
+++ Week_06/server.js
@@ -1,14 +1,26 @@
 const http = require('http')
-const fs = require('fs')
+// const fs = require('fs')
 
 http.createServer(function(req,res) {
-    if(req.url == '/favicon.ico') {
-        res.writeHead(200)
-        res.end()
-        return
-    }
-    res.writeHead(200)
-    fs.createReadStream(__dirname + '/index.html').pipe(res)
+    // if(req.url == '/favicon.ico') {
+    //     res.writeHead(200)
+    //     res.end()
+    //     return
+    // }
+    // res.writeHead(200)
+    // fs.createReadStream(__dirname + '/index.html').pipe(res)
+
+    let body = []
+    http.request.on('error',(err) => {
+        console.error(err)
+    }).on('data', (chunk) => {
+        body.push(chunk.toString())
+    }).on('end', () => {
+        body = Buffer.concat(body).toString()
+        console.log("body:",body)
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.end(' Hello World \n')
+    })
 }).listen(3001)
 
 console.log('server started')
