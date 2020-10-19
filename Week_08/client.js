6b3480e982eb379e5e26a660f85905f3c8baa0bb
zhangyoung99 <zhangyoung11@gmail.com>
2020-10-19 17:18:39 +0800

    根据浏览器元素排版

---
+++ Week_08/client.js
@@ -0,0 +1,156 @@
+// 代码直接copy第六周的代码
+
+const net = require("net")
+const parser = require("./parser.js")
+
+class Request {
+    constructor(options) {
+        this.method = option.method || "GET"
+        this.host = options.host
+        this.port = options.port || 80
+        this.path = options.path || "/"
+        this.body = options.body || {}
+        if(!this.headers["Content-Type"]) {
+            this.headers["Content-Type"] = "application/x-www-form-urlencoded"
+        }
+
+        if(this.headers["Content-Type"] === "application/json")
+           this.bodyText = JSON.stringify(this.body)
+        else if(this.headers["Content-Type"] === "application/x-www-form-urlencoded")
+           this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&')
+        
+        this.headers["Content-Length"] = this.bodyText.length
+    }
+    send(connection) {
+        return new Promise((resolve, reject) => {
+            // ......
+            const parser = new ResponseParser
+            if(connection) {
+                connection.write(this.toString())
+            } else {
+                connection = net.createConnection({
+                    host: this.host,
+                    port: this.port
+                },() => {
+                    connection.write(this.toString())
+                })
+            }
+            connection.on('data', (data) => {
+                parser.receive(data.toString())
+                if(parser.isFinished) {
+                    resolve(parser.response)
+                    connection.end()
+                }
+            })
+            connection.on('error' , (err) => {
+                reject(err)
+                connection.end()
+            })
+        })
+    }
+    toString() {
+        return `${this.method} ${this.path} HTTP/1.1\r
+        ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\r
+        ${this.bodyText}
+        `
+    }
+}
+
+class ResponseParser {
+    constructor() {
+        this.WAITING_STATUS_LINE = 0
+        this.WAITING_STATUS_LINE_END = 1
+        this.WAITING_HEADER_NAME = 2
+        this.WAITING_HEADER_SPACE = 3
+        this.WAITING_HEADER_VALUE = 4
+        this.WAITING_HEADER_LINE_END = 5
+        this.WAITING_HEADER_BLOCK_END = 6
+        this.WAITING_BODY = 7
+
+        this.current = this.WAITING_STATUS_LINE
+        this.statusLine = ''
+        this.headers = {}
+        this.headerName = ''
+        this.headerValue = ''
+        this.bodyParser = null
+    }
+    get isFinished() {
+        return this.bodyParser && this.bodyParser.isFinished;
+    }
+    receive(string) {
+        for(let i=0; i < string.length; i++) {
+            this.receiverChar(string.charAt(i))
+        }
+    }
+    receiveChar(char) {
+        if(this.current === this.WAITING_STATUS_LINE) {
+            if(char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END
+            } else {
+                this.statusLine += char
+            }
+        } else if(this.current === this.WAITING_STATUS_LINE_END) {
+            if(char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if(this.current === this.WAITING_HEADER_NAME) {
+            if(char === ':') {
+                this.current = this.WAITING_HEADER_SPACE
+            } else if(char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END
+                if(this.headers['Transfer-Encoding'] === 'chunked') {
+                    this.bodyParser = new TrunkedBodyParser()
+                }
+            } else {
+                this.headerName += char
+            }
+        } else if(this.current === this.WAITING_HEADER_SPACE) {
+            if(char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE
+            }
+        } else if(this.current === this.WAITING_HEADER_VALUE) {
+            if(char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END
+                this.headers[this.headerName] = this.headerValue
+                this.headerName = ''
+                this.headerValue = ''
+            } else {
+                this.headerValue += char
+            }
+        } else if(this.current === this.WAITING_HEADER_LINE_END) {
+            if(char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if(this.current === this.WAITING_HEADER_BLOCK_END) {
+            if(char === '\n') {
+                this.current = this.WAITING_BODY
+            }
+        } else if(this.current === this.WAITING_BODY) {
+            console.log(char)
+            // this.bodyParser.receiveChar(char);
+        }
+    }
+}
+
+
+void async function () {
+    let request = new Request({
+        method: "POST",
+        host: "127.0..0.1",
+        port: "8088",
+        path: "/",
+        headers: {
+            ["X-Foo2"]: "customed"
+        },
+        body: {
+            name: "zhangyoung"
+        }
+    })
+
+    let response = await request.send();
+
+    let dom = parser.parseHTML(response.body)
+
+    console.log(response,dom)
+}
+
---
+++ Week_08/server.js
@@ -0,0 +1,31 @@
+
+const http = require('http')
+
+server = http.createServer((req,res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('X-Foo', 'bar')
+    res.writeHead(200,{ 'Content-Type': 'text/plain'})
+    res.end(
+        `<html maaa=a>
+          <head>
+            <style>
+              body div #myid {
+                  width: 100px;
+                  background-color: #ff5000
+              }
+              body div img {
+                  width: 30px;
+              }
+            <style>
+         </header>
+         <body>
+           <div>
+              <img id="myid" />  
+              <img src="a" />
+           </div>
+
+         </body>`
+    )
+})
+
+server.listen(8080)
