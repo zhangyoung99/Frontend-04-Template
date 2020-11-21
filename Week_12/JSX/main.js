f3f88b31160f2454c4b36526b95d093587da4b8d
zhangyoung99 <zhangyoung11@gmail.com>
2020-11-21 17:28:08 +0800

    基本的轮播效果

--- Week_12/JSX/main.js
+++ Week_12/JSX/main.js
@@ -12,22 +12,46 @@ class Carousel extends Component {
     render() {
         console.log(this.attributes.src)
         this.root = document.createElement("div");
+        this.root.classList.add("carousel")
         for(let record of this.attributes.src) {
             let child = document.createElement("div")
             child.style.backgroundImage = `url('${record}')`
             this.root.appendChild(child)
         }
+        let currentIndex = 0;
+        setInterval(() => {
+            let children = this.root.children;
+
+            let nextIndex = (currentIndex + 1) % children.length;
+            // ++current;
+            // 在 1-n 之间循环，对 n 取余
+            // current =  current % children.length
+            let current = children[currentIndex];
+            let next = children[nextIndex];
+
+            next.style.transition = "none";
+            next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+            setTimeout(() => {
+                next.style.transition = '';
+                current.style.transform = `translateX(${ - 100 - currentIndex * 100}%)`
+                next.style.transform = `translateX(${- nextIndex * 100}%)`;
+
+                currentIndex = nextIndex
+                
+            }, 16)
+            // for(let child of children) {
+            //     child.style.transform = `translateX(-${current * 100}%)`;
+            // }
+            
+        },3000)
+
         return this.root
     }
     mountTo(parent) {
         parent.appendChild(this.render())
     }
 }
-
-// let a = <div id="a">
-//         <span>a</span>
-//         <span>b</span>
-//     </div>
 
 let d = [
     "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
