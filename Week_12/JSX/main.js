138b876df2b61ab4d1b73f92957f2b23d279a666
zhangyoung99 <zhangyoung11@gmail.com>
2020-11-22 11:31:33 +0800

    轮播step3

--- Week_12/JSX/main.js
+++ Week_12/JSX/main.js
@@ -10,7 +10,7 @@ class Carousel extends Component {
         this.attributes[name] = value;
     }
     render() {
-        console.log(this.attributes.src)
+        // console.log(this.attributes.src)
         this.root = document.createElement("div");
         this.root.classList.add("carousel")
         for(let record of this.attributes.src) {
@@ -18,33 +18,63 @@ class Carousel extends Component {
             child.style.backgroundImage = `url('${record}')`
             this.root.appendChild(child)
         }
-        let currentIndex = 0;
-        setInterval(() => {
-            let children = this.root.children;
 
-            let nextIndex = (currentIndex + 1) % children.length;
-            // ++current;
-            // 在 1-n 之间循环，对 n 取余
-            // current =  current % children.length
-            let current = children[currentIndex];
-            let next = children[nextIndex];
+        let position = 0;
 
-            next.style.transition = "none";
-            next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+        // 鼠标控制
+        this.root.addEventListener("mousedown",event => {
+            let children = this.root.children
+            let startX = event.clientX
 
-            setTimeout(() => {
-                next.style.transition = '';
-                current.style.transform = `translateX(${ - 100 - currentIndex * 100}%)`
-                next.style.transform = `translateX(${- nextIndex * 100}%)`;
+            let move = event => {
+                let x = event.clientX - startX
+                for(let child of children) {
+                    child.style.transition = "none"
+                    child.style.transform = `translateX(${- position * 500 + x}px)`
+                }
+            }
 
-                currentIndex = nextIndex
-                
-            }, 16)
-            // for(let child of children) {
-            //     child.style.transform = `translateX(-${current * 100}%)`;
-            // }
+            let up = event => {
+                let x = event.clientX - startX
+                position = position - Math.round( x / 500);
+                for(let child of children) {
+                    child.style.transition = "";
+                    child.style.transform = `translateX(${- position * 500}px)`
+                }
+                // mouseup不去响应 mousemove
+                document.removeEventListener('mouseomve',move)
+                document.removeEventListener('mouseup', up)
+            }
+
+            // mousedown 之后去响应 mousemove, mouseup之后不去响应 mousemove,调用上面的removeEventListener
+            this.root.addEventListener("mousemove", move)
+            this.root.addEventListener('mouseup',up)
+        })
+        // let currentIndex = 0;
+        // setInterval(() => {
+        //     let children = this.root.children;
+
+        //     let nextIndex = (currentIndex + 1) % children.length;
+        //     // ++current;
+        //     // 在 1-n 之间循环，对 n 取余
+        //     // current =  current % children.length
+        //     let current = children[currentIndex];
+        //     let next = children[nextIndex];
+
+        //     next.style.transition = "none";
+        //     next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+        //     setTimeout(() => {
+        //         next.style.transition = '';
+        //         current.style.transform = `translateX(${ - 100 - currentIndex * 100}%)`
+        //         next.style.transform = `translateX(${- nextIndex * 100}%)`;
+
+        //         currentIndex = nextIndex
+
+        //     }, 16)
             
-        },3000)
+        // },3000)
+
 
         return this.root
     }
