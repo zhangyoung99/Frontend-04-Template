a680d3874655a7d80bf4101d24885084df5f4568
zhangyoung99 <zhangyoung11@gmail.com>
2020-12-04 15:20:56 +0800

    添加动画暂停和重启功能

--- Week_13/JSX/animation.js
+++ Week_13/JSX/animation.js
@@ -3,7 +3,9 @@
 const TICK = Symbol("tick");
 const TICK_HANDLER = Symbol("tick-handler");
 const ANIMATIONS = Symbol("animations");
-const START_TIME = Symbol("start-time")
+const START_TIME = Symbol("start-time");
+const PAUSE_START = Symbol("pause-start");
+const PAUSE_TIME = Symbol("pause-time");
 
 export class Timeline {
     constructor() {
@@ -13,30 +15,35 @@ export class Timeline {
     }
     start() {
         let startTime =  Date.now();
+        this[PAUSE_TIME] = 0;
         // 调用自身
         this[TICK] = () => {
             let now =  Date.now();
             for(let animation of this[ANIMATIONS]) {
                 let t;
                 if(this[START_TIME].get(animation) < startTime)
-                   t = now - startTime
+                   t = now - startTime - this[PAUSE_TIME];
                 else 
-                   t = now - this[START_TIME].get(animation)
-                   
+                   t = now - this[START_TIME].get(animation) - this[PAUSE_TIME];
+
                 if(animation.duration < t) {
                     this[ANIMATIONS].delete(animation);
                     t = animation.duration;
                 }
                 animation.receive(t);
             }
-            requestAnimationFrame(this[TICK])
+            this[TICK_HANDLER] = requestAnimationFrame(this[TICK])
         }
         this[TICK]()
     }
     pause() {
-        cancelAnimationFrame(this[TICK_HANDLER])
+        this[PAUSE_START] = Date.now();
+        cancelAnimationFrame(this[TICK_HANDLER]);
     }
-    resume(){}
+    resume(){
+        this[PAUSE_TIME] += Date.now() - this[PAUSE_START];
+        this[TICK]()
+    }
     reset() {}
     add(animation,startTime) {
         if(arguments.length < 2) {
@@ -48,7 +55,7 @@ export class Timeline {
 }
 
 export class Animation {
-    constructor(object, property, startValue, endValue, duration, delay,timingFunction) {
+    constructor(object, property, startValue, endValue, duration, delay,timingFunction, template) {
         this.object = object;
         this.property = property;
         this.startValue = startValue;
@@ -56,10 +63,11 @@ export class Animation {
         this.duration = duration;
         this.timingFunction = timingFunction;
         this.delay = delay;
+        this.template = template;
     }
     receive(time) {
-        console.log(time)
-        let range = (this.endValue - this.startValue)
-        this.object[this.property] = this.startValue + range * time / this.duration
+        // console.log(time)
+        let range = (this.endValue - this.startValue);
+        this.object[this.property] = this.template(this.startValue + range * time / this.duration);
     }
 }
