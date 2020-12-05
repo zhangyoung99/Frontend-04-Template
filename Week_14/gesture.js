9e1f1c6deb84afd67b0835388b79bd0503e7c350
zhangyoung99 <zhangyoung11@gmail.com>
2020-12-05 21:10:25 +0800

    实现flick事件

--- Week_14/gesture.js
+++ Week_14/gesture.js
@@ -95,6 +95,11 @@ element.addEventListener("touchcancel", e => {
 let start = (point, context) => {
     // console.log("start", point.clientX, point.clientY)
     context.startX = point.clientX, context.startY = point.clientY;
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    }];
 
     context.isTap = true;
     context.isPan =  false;
@@ -125,6 +130,13 @@ let move = (point, context) => {
         console.log(dx, dy);
         console.log("pan");
     }
+
+    context.points = context.points.filter(point => Date.now() - point.t < 500)
+    context.points.push({
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY 
+    })
 }
 
 let end = (point, context) => {
@@ -137,6 +149,26 @@ let end = (point, context) => {
     }
     if(context.isPress) {
         console.log("pressend")
+    }
+
+    context.points = context.points.filter(point => Date.now() - point.t < 500);
+
+    let d, v;
+    if(!context.points.length) {
+        v = 0;
+    } else {
+        d =  Math.sqrt((point.clientX - context.points[0].x) ** 2 + 
+            (point.clientY - context.points[0].y) ** 2);
+
+        v = d / (Date.now() - context.points[0].t);
+    }
+
+
+    if(v > 1.5) {
+        console.log('flick');
+        context.isFlick = true;
+    } else {
+        context.isFlick = false;
     }
 }
 
