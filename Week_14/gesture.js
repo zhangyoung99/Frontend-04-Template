53cda74b09bc8b16910f6291f6d79ab94d85ba19
zhangyoung99 <zhangyoung11@gmail.com>
2020-12-05 20:17:16 +0800

    派发事件

--- Week_14/gesture.js
+++ Week_14/gesture.js
@@ -43,15 +43,15 @@ element.addEventListener("mousedown", e => {
         contexts.delete("mouse" + (1 << e.button));
 
         if(e.buttons === 0) {
-            element.removeEventListener("mousemove", mousemove);
-            element.removeEventListener("mouseup", mouseup);
+            document.removeEventListener("mousemove", mousemove);
+            document.removeEventListener("mouseup", mouseup);
             isListeningMouse = false;            
         }
 
     }
     if(!isListeningMouse) {
-        element.addEventListener("mousemove", mousemove)
-        element.addEventListener("mouseup", mouseup)  
+        document.addEventListener("mousemove", mousemove)
+        document.addEventListener("mouseup", mouseup)  
         isListeningMouse = true;      
     }
 })
@@ -129,7 +129,7 @@ let move = (point, context) => {
 
 let end = (point, context) => {
     if(context.isTap) {
-        console.log("tap");
+        dispatch("tap", {})
         clearTimeout(context.handler);
     }
     if(context.isPan) {
@@ -143,4 +143,14 @@ let end = (point, context) => {
 let cancel = (point, context) => {
     clearTimeout(context.handler);
     console.log("cancel", point.clientX, point.clientY)
+}
+
+// 派发事件
+function dispatch(type, properties) {
+    let event = new Event(type);
+    for(let name in properties) {
+        event[name] = properties[name];
+    }
+    element.dispatchEvent(event);
+
 }
